b79d745eedec56fd57f12e5c3c13a53f8a9b66fc
vishnu priya <vishnupriyalbk@gmail.com>
2026-06-29 19:22:32 +0530

    Add files via upload

---
+++ T0_PWM_CONTROL/GccApplication1/GccApplication1/main.c
@@ -0,0 +1,81 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 24-06-2026 10:55:55
+ * Author : hp
+ */ 
+
+#define F_CPU 16000000UL
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+uint8_t level = 0;
+
+
+void UpdatePWM(uint8_t level)
+{
+	switch(level)
+	{
+		case 0:
+		OCR0A = 64;      // 25%
+		break;
+
+		case 1:
+		OCR0A = 128;     // 50%
+		break;
+
+		case 2:
+		OCR0A = 191;     // 75%
+		break;
+
+		case 3:
+		OCR0A = 255;     // 100%
+		break;
+	}
+}
+
+int main(void)
+{
+	/* OC0A (PD6) as output */
+	DDRD |= (1 << PORTD6);
+
+	/* Push button on PD2 */
+	DDRD &= ~(1 << PORTD2);
+	PORTD |= (1 << PORTD2);
+
+	/* Timer0 Fast PWM, Non-Inverting */
+	TCCR0A = (1 << COM0A1) |
+	(1 << WGM01)  |
+	(1 << WGM00);
+
+	/* Prescaler = 64 */
+	TCCR0B = (1 << CS01) |
+	(1 << CS00);
+
+	UpdatePWM(level);
+
+	while(1)
+	{
+		if(!(PIND & (1 << PORTD2)))
+		{
+			_delay_ms(50);
+
+			if(!(PIND & (1 << PORTD2)))
+			{
+				//level++;
+
+				if(level < 3)
+				{
+					level++;
+				}
+
+				UpdatePWM(level);
+
+				while(!(PIND & (1 << PORTD2)));
+
+				_delay_ms(50);
+			}
+		}
+	}
+}
---
+++ TRAFFIC LIGHT CONTROLLER/GccApplication1/GccApplication1/main.c
@@ -0,0 +1,56 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 24-06-2026 10:34:07
+ * Author : hp
+ */ 
+
+#define F_CPU 16000000UL
+
+#include <avr/io.h>
+
+void Timer1_Delay_1s(void)
+{
+	TCCR1A = 0x00;
+
+	TCCR1B = (1 << WGM12);           // CTC Mode
+	OCR1A = 15624;                   // 1 second
+
+	TCNT1 = 0;
+	TIFR1 |= (1 << OCF1A);           // Clear flag
+
+	TCCR1B |= (1 << CS12) | (1 << CS10); // Prescaler 1024
+
+	while (!(TIFR1 & (1 << OCF1A)));
+
+	TCCR1B = 0x00;                   // Stop timer
+}
+
+void Delay_Seconds(uint8_t sec)
+{
+	while(sec--)
+	{
+		Timer1_Delay_1s();
+	}
+}
+
+int main(void)
+{
+	DDRB |= (1 << PORTB0) | (1 << PORTB1) | (1 << PORTB2);
+
+	while (1)
+	{
+		// Green ON
+		PORTB = (1 << PORTB0);
+		Delay_Seconds(10);
+
+		// Orange ON
+		PORTB = (1 << PORTB1);
+		Delay_Seconds(3);
+
+		// Red ON
+		PORTB = (1 << PORTB2);
+		Delay_Seconds(10);
+	}
+}
+
