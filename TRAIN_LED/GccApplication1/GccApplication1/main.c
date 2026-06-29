e5e81688ef42d049b02bc2e9e9c432d31f1b7a30
vishnu priya <vishnupriyalbk@gmail.com>
2026-06-29 19:19:14 +0530

    Add files via upload

---
+++ TRAIN_LED/GccApplication1/GccApplication1/main.c
@@ -0,0 +1,26 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 24-06-2026 10:16:18
+ * Author : hp
+ */ 
+
+#define F_CPU 16000000UL
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+	// Configure PORTB as output
+	DDRB = 0xFF;
+
+	while (1)
+	{
+		for (uint8_t i = 0; i < 8; i++)
+		{
+			PORTB = (1 << i);   // Turn ON only one LED
+			_delay_ms(200);     //  delay
+		}
+	}
+}
